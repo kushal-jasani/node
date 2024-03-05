0f3400b73668d3c17ddfc977df475a593a1dc8c3
kushal <kushaljasani@macbook-air.local>
2024-03-05 10:45:12 +0530

    completed using mongoose

--- controller/shop.js
+++ controller/shop.js
@@ -1,7 +1,7 @@
 const { clearCache } = require("ejs");
 const Product = require("../models/product");
 const { checkout } = require("../routes/admin");
-// const Order = require("../models/order");
+const Order = require("../models/order");
 
 exports.getProducts = (req, res, next) => {
   //   console.log(adminData.products);
@@ -90,10 +90,25 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
   req.user
-    .addOrder()
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user,
+        },
+        products: products,
+      });
+      return order.save();
+    })
     .then((result) => {
+      return req.user.clearCart();
+    })
+    .then(() => {
       res.redirect("/orders");
     })
     .catch((err) => console.log(err));
@@ -107,8 +122,7 @@ exports.getCheckOut = () => {
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({"user.userId":req.user._id})
     .then((orders) => {
       res.render("shop/orders", {
         path: "/orders",
--- models/user.js
+++ models/user.js
@@ -50,7 +50,12 @@ userSchema.methods.removeFromCart = function (productId) {
   const updatedCartItems = this.cart.items.filter((item) => {
     return item.productId.toString() !== productId.toString();
   });
-  this.cart.items=updatedCartItems;
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
+
+userSchema.methods.clearCart = function () {
+  this.cart = { items: [] };
   return this.save();
 };
 
